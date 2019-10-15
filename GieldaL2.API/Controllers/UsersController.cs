ef0c038a90c0336c320e11ac8e72d7cdcab40b6e
Paweł Osikowski <tearthdev@gmail.com>
2019-10-15 23:27:44 +0200

    Add User and Users endpoints

---
+++ GieldaL2.API/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using GieldaL2.API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GieldaL2.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<UserViewModel>> Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ GieldaL2.API/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GieldaL2.API.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DefaultValue(0)]
+        public decimal Value { get; set; }
+    }
+}
