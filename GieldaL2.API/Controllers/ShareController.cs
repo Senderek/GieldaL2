a813df06dc9491e570ae79e3a447664f428413d9
Paweł Osikowski <tearthdev@gmail.com>
2019-10-16 12:31:43 +0200

    Add Share and Shares endpoints

---
+++ GieldaL2.API/Controllers/ShareController.cs
@@ -0,0 +1,40 @@
+﻿using GieldaL2.API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GieldaL2.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ShareController
+    {
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<string> Get(int id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(500)]
+        public void Post([FromBody] ShareViewModel share)
+        {
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public void Put(int id, [FromBody] ShareViewModel share)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(500)]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GieldaL2.API/Controllers/SharesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using GieldaL2.API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GieldaL2.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class SharesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<ShareViewModel>> Get()
+        {
+            return null;
+        }
+    }
+}
