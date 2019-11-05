40ea5655e0aed94e0e6cf2517d44f55faf13de2b
Miłosz Osadczuk <m2ilosz@outlook.com>
2019-11-05 22:43:41 +0100

    Task 27: Klasy modelu danych

---
+++ GieldaL2.DB/Entities/Share.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace GieldaL2.DB.Entities
+{
+	/// <summary>
+	/// Represents some number of shares of a company owned by a particular user 
+	/// </summary>
+	public class Share
+	{
+		public int Id { get; set; }
+		/// <summary>
+		/// Owner of the shares
+		/// </summary>
+		virtual public User Owner { get; set; }
+		public int Amount { get; set; }
+		/// <summary>
+		/// Represents a company which the shares represent
+		/// </summary>
+		virtual public Stock Stock { get; set; } 
+	}
+}
--- GieldaL2.DB/Entities/Stock.cs
+++ GieldaL2.DB/Entities/Stock.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GieldaL2.DB.Entities
 {
     public class Stock
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Abbreviation { get; set; }
+		virtual public List<Share> Shares { get; set; }
+		/// <summary>
+		/// Current computed value of single share of this company
+		/// </summary>
+		public decimal CurrentPrice { get; set; }
     }
 }
