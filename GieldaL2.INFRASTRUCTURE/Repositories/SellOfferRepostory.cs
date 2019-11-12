e8324ea7a46f984dc3b964e2b324746d3853125d
Tomasz Wąż <tomaszw1995@gmail.com>
2019-11-12 14:29:09 +0100

    Added repositories

---
+++ GieldaL2.INFRASTRUCTURE/Repositories/SellOfferRepostory.cs
@@ -0,0 +1,43 @@
+﻿using GieldaL2.DB;
+using GieldaL2.DB.Entities;
+using GieldaL2.DB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GieldaL2.INFRASTRUCTURE.Repositories
+{
+    public class SellOfferRepostory : ISellOfferRepository
+    {
+        private readonly GieldaL2Context _context;
+
+        public SellOfferRepostory(GieldaL2Context context)
+        {
+            _context = context;
+        }
+
+
+        public SellOffer GetById(int id)
+        {
+            return _context.SellOffers.FirstOrDefault(selloffer => selloffer.Id == id);
+        }
+
+        public ICollection<SellOffer> GetAll()
+        {
+            return _context.SellOffers.ToList();
+        }
+
+
+        public void Add(SellOffer sellOffer)
+        {
+            _context.SellOffers.Add(sellOffer);
+            _context.SaveChanges();
+        }
+
+        public void Remove(SellOffer sellOffer)
+        {
+            _context.Remove(sellOffer);
+            _context.SaveChanges();
+        }
+    }
+}
