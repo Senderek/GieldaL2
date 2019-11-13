dab18806afd3f7fbc33c5030b957c4f368541946
Miłosz Osadczuk <m2ilosz@outlook.com>
2019-11-13 21:24:23 +0100

    Task 49: Implementation of API for Stocks

--- GieldaL2.API/Controllers/StocksController.cs
+++ GieldaL2.API/Controllers/StocksController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using GieldaL2.API.ViewModels.Edit;
 using GieldaL2.API.ViewModels.View;
+using GieldaL2.DB;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GieldaL2.API.Controllers
@@ -10,39 +13,103 @@ namespace GieldaL2.API.Controllers
     [Produces("application/json")]
     public class StocksController : ControllerBase
     {
+		GieldaL2Context context;
+		public StocksController(GieldaL2Context context)
+		{
+			this.context = context;
+		}
+
         [HttpGet]
         [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public ActionResult<IEnumerable<StockViewModel>> Get()
+        public ActionResult<StatisticsViewModel<IEnumerable<StockViewModel>>> Get()
         {
-            return null;
+			return new StatisticsViewModel<IEnumerable<StockViewModel>>()
+			{
+				//TODO: statistics
+				Data = (from s in context.Stocks
+						select new StockViewModel()
+						{
+							Id = s.Id,
+							Name = s.Name,
+							Abbreviation = s.Abbreviation,
+							CurrentPrice = s.CurrentPrice,
+							PriceDelta = s.PriceDelta
+						}).ToList()
+			};
         }
 
-        [HttpGet("{id}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(500)]
-        public ActionResult<StockViewModel> Get(int id)
-        {
-            return null;
-        }
+		[HttpGet("{id}")]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(404)]
+		[ProducesResponseType(500)]
+		public ActionResult<StatisticsViewModel<StockViewModel>> Get(int id)
+		{
+			var s = context.Stocks.Find(id);
+			return new StatisticsViewModel<StockViewModel>()
+			{
+				//TODO: statistics
+				Data = new StockViewModel()
+				{
+					Id = s.Id,
+					Name = s.Name,
+					Abbreviation = s.Abbreviation,
+					CurrentPrice = s.CurrentPrice,
+					PriceDelta = s.PriceDelta
+				}
+			};
+		}
 
         [HttpPost]
         [ProducesResponseType(201)]
         [ProducesResponseType(500)]
-        public ActionResult<StatisticsViewModel> Post([FromBody] EditStockViewModel user)
+        public ActionResult<StatisticsViewModel> Post([FromBody] EditStockViewModel model)
         {
-            return null;
+			DB.Entities.Stock stock = new DB.Entities.Stock()
+			{
+				Name = model.Name,
+				Abbreviation = model.Abbreviation,
+				//CurrentPrice = model.CurrentPrice,
+				//PriceDelta = model.PriceDelta
+			};
+
+			context.Stocks.Add(stock);
+			context.SaveChangesAsync();
+            return new StatisticsViewModel()
+				{
+				//TODO: statistics
+			};
         }
 
         [HttpPut("{id}")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
-        public ActionResult<StatisticsViewModel> Put(int id, [FromBody] EditStockViewModel user)
+        public async Task<ActionResult<StatisticsViewModel>> Put(int id, [FromBody] EditStockViewModel model)
         {
-            return null;
-        }
+			DB.Entities.Stock stock = context.Stocks.Find(id);
+
+			if (stock == null) return NotFound();
+
+			stock.Name = model.Name;
+			stock.Abbreviation = model.Abbreviation;
+			//TODO: stock.CurrentPrice = 
+			//TODO: stock.PriceDelta = 
+
+			if (await TryUpdateModelAsync<DB.Entities.Stock>(stock))
+			{
+				await context.SaveChangesAsync();
+
+				return new StatisticsViewModel()
+				{
+					//TODO: statistics
+				};
+			}
+			else
+			{
+				return StatusCode(500);
+			}
+		}
 
         [HttpDelete("{id}")]
         [ProducesResponseType(204)]
@@ -50,7 +117,16 @@ namespace GieldaL2.API.Controllers
         [ProducesResponseType(500)]
         public ActionResult<StatisticsViewModel> Delete(int id)
         {
-            return null;
-        }
+			DB.Entities.Stock stock = context.Stocks.Find(id);
+
+			if (stock == null) return NotFound();
+
+			context.Stocks.Remove(stock);
+			context.SaveChangesAsync();
+			return new StatisticsViewModel()
+			{
+				//TODO: statistics
+			};
+		}
     }
 }
--- GieldaL2.API/ViewModels/View/StockViewModel.cs
+++ GieldaL2.API/ViewModels/View/StockViewModel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Abbreviation { get; set; }
-
-        public StatisticsViewModel Statistics { get; set; }
-    }
+		public decimal CurrentPrice { get; set; }
+		public decimal PriceDelta { get; set; }
+	}
 }
