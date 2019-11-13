752cd6f86d2c5134f7efecfc473b4279173aebc6
Miłosz Osadczuk <m2ilosz@outlook.com>
2019-11-13 21:00:42 +0100

    Add StatisticsViewModel<T> with Data

--- GieldaL2.API/ViewModels/View/StatisticsViewModel.cs
+++ GieldaL2.API/ViewModels/View/StatisticsViewModel.cs
@@ -10,4 +10,17 @@
         public float UpdatesTime { get; set; }
         public int UpdatesCount { get; set; }
     }
+
+	public class StatisticsViewModel<T>
+	{
+		public float BackendTime { get; set; }
+
+		public float SelectsTime { get; set; }
+		public int SelectsCount { get; set; }
+
+		public float UpdatesTime { get; set; }
+		public int UpdatesCount { get; set; }
+
+		public T Data { get; set; }
+	}
 }
