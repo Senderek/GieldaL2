81c8361854f959bb0999921565e08a65cfb5b734
Paweł Osikowski <tearthdev@gmail.com>
2019-11-19 15:58:16 +0100

    Add comments to the backend time filter

--- GieldaL2.API/Filters/BackendTimeFilter.cs
+++ GieldaL2.API/Filters/BackendTimeFilter.cs
@@ -4,20 +4,31 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace GieldaL2.API.Filters
 {
+    /// <summary>
+    /// Filter calculating total time spent on the backend logic.
+    /// </summary>
     public class BackendTimeFilter : IActionFilter
     {
         private readonly Stopwatch _stopWatch = new Stopwatch();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OnActionExecuting"/> class.
+        /// </summary>
+        /// <param name="filterContext">Filter context passed by the ASP engine.</param>
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
             _stopWatch.Reset();
             _stopWatch.Start();
         }
 
-        public void OnActionExecuted(ActionExecutedContext filterContext)
+        /// <summary>
+        /// Called after the action executes, before the action result.
+        /// </summary>
+        /// <param name="context">The <see cref="T:Microsoft.AspNetCore.Mvc.Filters.ActionExecutedContext" />.</param>
+        public void OnActionExecuted(ActionExecutedContext context)
         {
             var executionTime = (int)_stopWatch.ElapsedMilliseconds;
-            var responseObject = (ObjectResult)filterContext.Result;
+            var responseObject = (ObjectResult)context.Result;
             var backendTimeProperty = responseObject.Value.GetType().GetProperty("BackendTime");
 
             backendTimeProperty?.SetValue(responseObject.Value, executionTime);
