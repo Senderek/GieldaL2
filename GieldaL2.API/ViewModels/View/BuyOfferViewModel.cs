f0cc601f48b45952c84f7d2ba9e714b9f8ed8fd3
Piotr Świerad <piotrswierad.mail@gmail.com>
2019-12-02 16:22:44 +0100

    Fixed documentation path and modified buy offer view model to match existing table

--- GieldaL2.API/ViewModels/View/BuyOfferViewModel.cs
+++ GieldaL2.API/ViewModels/View/BuyOfferViewModel.cs
@@ -5,7 +5,7 @@ namespace GieldaL2.API.ViewModels.View
     public class BuyOfferViewModel
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public int BuyerId { get; set; }
         public int StockId { get; set; }
         public int Amount { get; set; }
         public decimal Price { get; set; }
